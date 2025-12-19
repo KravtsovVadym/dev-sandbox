3bed39e93f7790a5a0d8584e241f4f98f91542c8
Vadym <vadymkravtcsov@gmail.com>
2025-12-19 17:31:03 +0200

    add function Task_24.py 'Created decorator which measures time'

---
+++ function/Task_24.py
@@ -0,0 +1,14 @@
+""" This is decorator function that measuresprogram time """
+
+import time
+from functools import wraps
+
+def measure_time(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function {func.__name__} took {end_time - start_time} seconds to execute.")
+        return result
+    return wrapper
